e2e4c02d23a5cbb7813e19c83d8d360d5cfed3c2
najindra <najindraprasad@gmail.com>
2018-07-19 12:28:50 +0530

    Update data.js

--- data.js
+++ data.js
@@ -1,20 +1,44 @@
-function getfile(file,callback){
-  var xhr = new XMLHttpRequest();
-  xhr.overrideMimeType("application/json");
-  xhr.open("GET",file,true);
-  xhr.onreadystatechange = function(){
-    if(xhr.readyState === 4 && xhr.status == "200"){
-      callback(xhr.responseText);
-    }
-  };
-  xhr.send();
+// function getfile(file,callback){
+//   var xhr = new XMLHttpRequest();
+//   xhr.overrideMimeType("application/json");
+//   xhr.open("GET",file,true);
+//   xhr.onreadystatechange = function(){
+//     if(xhr.readyState === 4 && xhr.status == "200"){
+//       callback(xhr.responseText);
+//     }
+//   };
+//   xhr.send();
+// }
+// getfile("data.json",function(text) {
+//   var data = JSON.parse(text);
+//   console.log(data);
+//   details(data.basics);
+//   career(data.CareerObjective);
+//   education(data.education);
+//   skill(data.skills);
+//   achieve(data.achievements);
+// } )
+
+function loadjson(file){
+  return new Promise((resolve,reject)=>{
+    return fetch(file).then(response=>{
+      if(response.ok){
+        resolve(response.json());
+      }else{
+        reject(new Error('error'));
+      }
+    })
+  })
 }
-getfile("data.json",function(text) {
-  var data = JSON.parse(text);
-  console.log(data);
+
+var newfile = loadjson("data.json")
+newfile.then(data=>{
   details(data.basics);
   career(data.CareerObjective);
-} )
+  education(data.education);
+  skill(data.skills);
+  achieve(data.achievements);
+})
 
 var child = document.querySelector(".childone");
 
@@ -62,6 +86,64 @@ function career(careerinfo){
   var info = document.createElement("i");
   info.textContent = careerinfo.info;
   child2.appendChild(info);
+}
+  function education(edu){
+  var add = document.createElement("h3");
+  add.textContent = "Education Qualification";
+  child2.appendChild(add);
 
+  var hr = document.createElement("hr");
+  child2.appendChild(hr);
 
+  for(i=0;i<edu.length;i++){
+    var deg = document.createElement("h3");
+    deg.textContent = edu[i].degree;
+    child2.appendChild(deg);
+
+    var eduul = document.createElement("ul");
+    var eduli = document.createElement("li");
+    eduli.textContent = edu[i].institute;
+    eduul.appendChild(eduli);
+    child2.appendChild(eduul);
+
+    var eduul = document.createElement("ul");
+    var eduli = document.createElement("li");
+    eduli.textContent = edu[i].data;
+    eduul.appendChild(eduli);
+    child2.appendChild(eduul);
+  }
+}
+function skill(skillinfo){
+var add = document.createElement("h3");
+add.textContent = "Technical Skills";
+child2.appendChild(add);
+
+var hr = document.createElement("hr");
+child2.appendChild(hr);
+
+  var skilldata = document.createElement("table");
+  skilldata.border = "3";
+  child2.appendChild(skilldata);
+
+  tabledata="";
+  for(i=0;i<skillinfo.length;i++){
+    tabledata+="<tr><td>"+skillinfo[i].title+"</td><td>"+skillinfo[i].data+"</td></tr>";
+  }
+  skilldata.innerHTML = tabledata;
+}
+function achieve(ach){
+  var add = document.createElement("h3");
+  add.textContent = "Achievements";
+  child2.appendChild(add);
+
+  var hr = document.createElement("hr");
+  child2.appendChild(hr);
+
+  for(i=0;i<ach.length;i++){
+    var eduul = document.createElement("ul");
+    var eduli = document.createElement("li");
+    eduli.textContent = ach[i].data;
+    eduul.appendChild(eduli);
+    child2.appendChild(eduul);
+}
 }
